d24eabd69043f838cf47d36cdebc3478ab4f334f
seshadri <seshadrireddy073@gmail.com>
2024-05-25 13:35:01 +0530

    Added refreshScope to Employee and Domain services to get lastest properties from Config repo without config server restart

---
+++ DomainDetails/src/main/java/com/spring/learn/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.spring.learn.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController {
+	
+	@Value("${spring.boot.message}")
+	private String value;
+	
+	@RequestMapping("/hi")
+	public ResponseEntity<String> hiMessage(){
+		return new ResponseEntity<String>(value,HttpStatus.OK);
+	}
+}
---
+++ EmployeeDetails/src/main/java/com/spring/learn/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.spring.learn.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController {
+	@Value("${spring.boot.messagevalue}")
+	private String message;
+	@RequestMapping("/hi")
+	public ResponseEntity<String> hiMessage(){
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+	}
+}
