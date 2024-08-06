ac5d5a87e9c7447ab37087a863375ed79840aeae
seshadri <seshadrireddy073@gmail.com>
2024-08-06 12:56:12 +0530

    Customized username and password for security via property file

--- SpringSecExmpl/src/main/java/com/spring/secex/controller/Restcontroller.java
+++ SpringSecExmpl/src/main/java/com/spring/secex/controller/Restcontroller.java
@@ -5,25 +5,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.secex.model.Student;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
 public class Restcontroller {
+	
+	private Student obj = new Student();
 	@GetMapping("/status")
 	public ResponseEntity<String> status(HttpServletRequest request) {
 		return new ResponseEntity<String>("I am available ::"+request.getSession().getId(),HttpStatus.OK);
 	}
 	@PostMapping("/add")
-	public ResponseEntity<String> add(HttpServletRequest request) {
-		return new ResponseEntity<String>("Record added successfully ::"+request.getSession().getId(),HttpStatus.OK);
+	public ResponseEntity<Student> add(@RequestBody Student student, HttpServletRequest request) {
+		obj.setId(student.getId());
+		obj.setGrade(student.getGrade());
+		obj.setName(student.getName());
+		obj.setGuardian(student.getGuardian());
+		return new ResponseEntity<Student>(obj,HttpStatus.OK);
 	}
 	@GetMapping("/csrf")
 	public CsrfToken csrftoken(HttpServletRequest request) {
 		return (CsrfToken) request.getAttribute("_csrf") ;
+		
+	}
+	@GetMapping("/all")
+	public ResponseEntity<Student> getAll() {
+		return new ResponseEntity<Student>(obj,HttpStatus.OK);
 		
 	}
 }
---
+++ SpringSecExmpl/src/main/java/com/spring/secex/model/Student.java
@@ -0,0 +1,12 @@
+package com.spring.secex.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	
+	private Integer id;
+	private String name;
+	private String grade;
+	private String guardian;
+}
