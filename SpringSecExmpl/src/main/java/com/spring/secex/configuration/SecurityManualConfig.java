c8d9b2989d4054318dd7147778f1f9c120518af2
seshadri <seshadrireddy073@gmail.com>
2024-08-06 21:51:37 +0530

    Reading the username and properties from userDetailserver class and implemented authorization and authentication

--- SpringSecExmpl/src/main/java/com/spring/secex/configuration/SecurityManualConfig.java
+++ SpringSecExmpl/src/main/java/com/spring/secex/configuration/SecurityManualConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,15 +19,28 @@ public class SecurityManualConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     	http
     		.csrf(customizer->customizer.disable())
-    		.authorizeHttpRequests(request->request.anyRequest().authenticated())
-				/*
-				 * .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-				 * .requestMatchers("/admin/**").hasRole("ADMIN")
-				 * .requestMatchers("/**").hasRole("USER") )
-				 */
+    		//.authorizeHttpRequests(request->request.anyRequest().authenticated())
+			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/api/admin/**")
+						.hasRole("ADMIN").requestMatchers("/**").hasRole("USER"))
 			//.formLogin(withDefaults()) -- we commented because at browser will ask again and again username and passoword so using basicHttp we can go
     		.httpBasic(withDefaults())
     		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
 	}
+    
+    @Bean
+    public UserDetailsService userDetailService() {
+    		UserDetails user1 = User.withDefaultPasswordEncoder()
+    								.username("seshadri")
+    								.password("s@123")
+    								.roles("USER")
+    								.build();
+    		UserDetails user2 = User.withDefaultPasswordEncoder()
+									.username("Ram")
+									.password("r@123")
+									.roles("ADMIN")
+									.build();
+		return new InMemoryUserDetailsManager(user1,user2);
+    	
+    }
 }
